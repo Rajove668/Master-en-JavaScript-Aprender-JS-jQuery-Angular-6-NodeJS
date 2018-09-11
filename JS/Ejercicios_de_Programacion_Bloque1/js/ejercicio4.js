7c93c539cab8e2064782dcbd1573aa470d1466f1
Ramiro Jose Verbel De La Rosa <rajove668@hotmail.com>
2018-09-11 15:14:25 -0500

    Ejercicios de Programación en JS
    Bloque 1, ejercicio 4 completo

--- JS/Ejercicios_de_Programacion_Bloque1/js/ejercicio4.js
+++ JS/Ejercicios_de_Programacion_Bloque1/js/ejercicio4.js
@@ -1,5 +1,27 @@
 'use strict'
 
 /*
-
+Mostrar los numeros impares entre dos numeros introducidos por el usuario
 */
+
+var aux = true;
+do {
+    var num1 = parseInt(prompt("Introduce el primer numero: ", 0));
+    var num2 = parseInt(prompt("Introduce el segundo numero: ", 0));
+
+    if (isNaN(num1 * num2)) {
+        alert("Introduce numeros correctos");
+    } else if (num2 < num1) {
+        alert("El segundo numero debe ser mayor que el primero");
+    } else {
+        document.write("<h1> De " + num1 + " a " + num2 + " estan los siguientes numeros impares:</h1>")
+        for (let index = num1; index <= num2; index++) {
+            if (index % 2 != 0) {
+                console.log(index);
+                document.write(index + "<br>");
+            }
+        }
+        aux = false;
+    }
+
+} while (aux);
