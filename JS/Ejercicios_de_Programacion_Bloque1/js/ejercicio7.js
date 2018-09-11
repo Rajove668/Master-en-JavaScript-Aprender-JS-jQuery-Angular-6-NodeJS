c51448841fdc81251730a0dda94304c29e80e59c
Ramiro Jose Verbel De La Rosa <rajove668@hotmail.com>
2018-09-11 15:48:47 -0500

    Ejercicios de Programación en JS
    Bloque 1, ejercicio 7 completo

--- JS/Ejercicios_de_Programacion_Bloque1/js/ejercicio7.js
+++ JS/Ejercicios_de_Programacion_Bloque1/js/ejercicio7.js
@@ -1,5 +1,20 @@
 'use stritc'
 
 /*
-
+Tabla de multiplicar de Un numero introducido por pantalla.
 */
+
+
+var num = parseInt(prompt("¿De que numero quieres la tabla?", 1));
+
+for (let index = 1; index <= 10; index++) {
+    console.log(index + " X " + num + " = " + (index * num));
+}
+
+//Todas las tablas
+for (let index1 = 0; index1 <= 10; index1++) {
+    for (let index2 = 0; index2 <= 10; index2++) {
+        console.log(index2 + " X " + index1 + " = " + (index2 * index1));
+    }
+}
+
