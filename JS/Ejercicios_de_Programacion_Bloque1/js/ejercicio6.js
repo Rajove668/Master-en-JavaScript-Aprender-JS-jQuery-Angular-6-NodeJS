1abd4ff38faee99d83ffc27a4c14eb61c5aa1c57
Ramiro Jose Verbel De La Rosa <rajove668@hotmail.com>
2018-09-11 15:38:49 -0500

    Ejercicios de Programación en JS
    Bloque 1, ejercicio 6 completo

--- JS/Ejercicios_de_Programacion_Bloque1/js/ejercicio6.js
+++ JS/Ejercicios_de_Programacion_Bloque1/js/ejercicio6.js
@@ -1,5 +1,23 @@
 'use strict'
 
 /*  
-
+ Programa que nos diga si un numero es par o impar
+ 1. Ventana prompt
+ 2. Si un num no es valido que lo pida  de nuevo
 */
+
+var aux;
+do {
+    aux = false;
+    var num = parseInt(prompt("Introduce un numero para saber si es par o impar: ", 0));
+    if (isNaN(num)) {
+        alert("Ingrese un Numero Valido");
+        aux = true;
+    }
+} while (aux);
+
+if (num % 2 == 0) {
+    console.log("Es Par");
+} else {
+    console.log("Es Impar");
+}
