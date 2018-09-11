bbc3dac80cbe84e4d7c8fa9f7dec38227651004d
Ramiro Jose Verbel De La Rosa <rajove668@hotmail.com>
2018-09-11 15:25:34 -0500

    Ejercicios de Programación en JS
    Bloque 1, ejercicio 5 completo

--- JS/Ejercicios_de_Programacion_Bloque1/js/ejercicio5.js
+++ JS/Ejercicios_de_Programacion_Bloque1/js/ejercicio5.js
@@ -1,5 +1,13 @@
 'use strict'
 
 /* 
-
+Muestre todos los numeros divisores de un numero introducido en un prompt
 */
+
+var num = parseInt(prompt("Introduce un numero: ", 1));
+
+for (let index = 1; index <= num; index++) {
+    if (num % index == 0) {
+        console.log("Divisor: " + index);
+    }
+}
