297e09af3231421486143a187b3728e37be8489d
Ramiro Jose Verbel De La Rosa <rajove668@hotmail.com>
2018-09-11 11:32:04 -0500

    Finaliza Sección 10
    Alertas y Ventanas

---
+++ JS/hola-mundo/js/ventanas.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// Alerta
+
+alert("¡Esto es una Alerta!");
+
+// Alerta de Confirmación
+
+var mi_resultado = confirm("¿Estas seguro de continuar esta alerta de confirmacíon?");
+
+console.log("La decición tomado fue: " + mi_resultado);
+
+// Alerta de Ingreso de Datos
+
+var mi_resultado2 = prompt("¿Que edad tienes?", 18); // prompt siempre retorna String
+
+console.log("La edad escrita es: " + mi_resultado2);
